16179169151aceef06763d29737d212a8b446ef2
skjyrte <wasylewiczkrzysztof@gmail.com>
2024-07-25 10:27:04 +0200

    refactor: update imports, modularize functions, and improve reliability
    1. gender.ts
    - Updated imports to reflect changes in module organization.
    - Introduced processFilterPath module into the route for better processing.
    - Removed joinVariants function from the route body and moved it to a separate module.
    - Refactored code for enhanced reliability and maintainability.
    
    2. joinSqlQuery.ts
    - Moved joinVariants function from route to this separate module for better modularity.
    - Refactored code to improve reliability and readability.
    - Modified input parameters for more flexibility and better function usage.
    
    3. joinSqlQuery.test.ts
    - Created tests for joinSqlQuery

--- src/routes/gender.ts
+++ src/routes/gender.ts
@@ -1,9 +1,10 @@
 import express from "express";
 import pool from "../db.js";
-import processSQLProductBasicData from "./routerUtilities/processSQLProductBasicData";
-import createResponse from "./routerUtilities/createResponse";
-import processVariants from "./routerUtilities/processFilterPath.js";
-import createQueryArray from "./routerUtilities/createQueryArray";
+import processSQLProductBasicData from "./routerUtilities/processSQLProductBasicData.js";
+import createResponse from "./routerUtilities/createResponse.js";
+import {processFilterPath} from "./routerUtilities/processFilterPath.js";
+import {joinSqlQuery} from "./routerUtilities/joinSqlQuery.js";
+import createQueryArray from "./routerUtilities/createQueryArray.js";
 
 const router = express.Router();
 
@@ -80,57 +81,47 @@ router.get("/:gender/category/:category/:variants", async (req, res) => {
   const {gender, category, variants} = req.params;
   const {material} = req.query;
 
-  /* create arrays */
-  const {brandsArray, colorsArray, sizesArray} = processVariants(variants);
-  const materialsArray = createQueryArray(material);
-
   try {
-    let query = `
-    SELECT 
-      products.*, 
-      product_stock.size, 
-      product_stock.count, 
-      product_images.image_url
-    FROM 
-      products
-    LEFT JOIN 
-      product_stock ON products.id = product_stock.product_id
-    LEFT JOIN 
-      product_images ON products.id = product_images.product_id
+    const objToCheck = processFilterPath(variants);
+
+    if (!objToCheck) {
+      throw new Error("Error during processing filter path");
+    }
+    const {brandsArray, colorsArray, sizesArray} = objToCheck;
+
+    const materialsArray = createQueryArray(material);
+    const query = `
+      SELECT
+        products.*,
+        product_stock.size,
+        product_stock.count,
+        product_images.image_url
+      FROM
+        products
+      LEFT JOIN
+        product_stock ON products.id = product_stock.product_id
+      LEFT JOIN
+        product_images ON products.id = product_images.product_id
       WHERE products.id IN (
-      SELECT DISTINCT products.id
-      FROM products
-      LEFT JOIN product_stock ON products.id = product_stock.product_id
-      WHERE 1=1 AND products.gender = $1 AND products.category = $2`;
+        SELECT products.id
+        FROM products
+        LEFT JOIN product_stock ON products.id = product_stock.product_id
+        WHERE 1=1 
+          AND products.gender = $1 
+          AND products.category = $2 `;
 
     const queryParams: string[] = [gender, category];
-    let paramIndex = queryParams.length + 1;
 
-    const joinVariants = (queryArray: string[], queryTitle: string) => {
-      let subQuery = "";
-      queryArray.forEach((q, i) => {
-        if (i === 0) {
-          subQuery += ` AND (${queryTitle} = $${paramIndex++}`;
-          queryParams.push(q);
-        } else {
-          subQuery += ` OR ${queryTitle} = $${paramIndex++}`;
-          queryParams.push(q);
-        }
-      });
-      if (subQuery.length !== 0) {
-        subQuery += ")";
-      }
-      query += subQuery;
-    };
+    const dataToExec = [
+      {filterArray: sizesArray, filterParameter: "product_stock.size"},
+      {filterArray: colorsArray, filterParameter: "products.color"},
+      {filterArray: brandsArray, filterParameter: "products.brand"},
+      {filterArray: materialsArray, filterParameter: "products.material"},
+    ];
 
-    joinVariants(sizesArray, "product_stock.size");
-    joinVariants(colorsArray, "products.color");
-    joinVariants(brandsArray, "products.brand");
-    joinVariants(materialsArray, "products.material");
+    const override = joinSqlQuery(query, [...queryParams], [...dataToExec]);
 
-    query += ")";
-
-    const result = await client.query(query, queryParams);
+    const result = await client.query(override.query, override.queryParams);
 
     if (result.rows.length > 0) {
       const processedResult = processSQLProductBasicData(result.rows);
---
+++ tests/joinSqlQuery.test.ts
@@ -0,0 +1,80 @@
+import {describe, expect} from "@jest/globals";
+import {
+  joinSqlQuery,
+  SqlQueryInput,
+} from "../src/routes/routerUtilities/joinSqlQuery";
+
+describe("processFilterPath: ", () => {
+  it("basic test", () => {
+    const baseSql = `
+    SELECT 
+      products.*, 
+      product_stock.size, 
+      product_stock.count, 
+      product_images.image_url
+    FROM 
+      products
+    LEFT JOIN 
+      product_stock ON products.id = product_stock.product_id
+    LEFT JOIN 
+      product_images ON products.id = product_images.product_id
+      WHERE products.id IN (
+      SELECT DISTINCT products.id
+      FROM products
+      LEFT JOIN product_stock ON products.id = product_stock.product_id
+      WHERE 1=1 AND products.gender = $1 AND products.category = $2`;
+
+    const queryParams: string[] = ["gender", "category"];
+
+    const sizesArray = ["40", "41", "46", "48"];
+    const colorsArray = ["red", "white", "black"];
+    const brandsArray = ["Zalton", "UrbanStep"];
+    const materialsArray = ["mesh"];
+    const dataToExec = [
+      {filterArray: sizesArray, filterParameter: "product_stock.size"},
+      {filterArray: colorsArray, filterParameter: "products.color"},
+      {filterArray: brandsArray, filterParameter: "products.brand"},
+      {filterArray: materialsArray, filterParameter: "products.material"},
+    ];
+
+    const result = joinSqlQuery(baseSql, [...queryParams], [...dataToExec]);
+
+    console.log(result);
+
+    const expectedOutput: SqlQueryInput = {
+      query:
+        "\n" +
+        "    SELECT \n" +
+        "      products.*, \n" +
+        "      product_stock.size, \n" +
+        "      product_stock.count, \n" +
+        "      product_images.image_url\n" +
+        "    FROM \n" +
+        "      products\n" +
+        "    LEFT JOIN \n" +
+        "      product_stock ON products.id = product_stock.product_id\n" +
+        "    LEFT JOIN \n" +
+        "      product_images ON products.id = product_images.product_id\n" +
+        "      WHERE products.id IN (\n" +
+        "      SELECT DISTINCT products.id\n" +
+        "      FROM products\n" +
+        "      LEFT JOIN product_stock ON products.id = product_stock.product_id\n" +
+        "      WHERE 1=1 AND products.gender = $1 AND products.category = $2 AND (product_stock.size = $3 OR product_stock.size = $4 OR product_stock.size = $5 OR product_stock.size = $6) AND (products.color = $7 OR products.color = $8 OR products.color = $9) AND (products.brand = $10 OR products.brand = $11) AND (products.material = $12))",
+      queryParams: [
+        "gender",
+        "category",
+        "40",
+        "41",
+        "46",
+        "48",
+        "red",
+        "white",
+        "black",
+        "Zalton",
+        "UrbanStep",
+        "mesh",
+      ],
+    };
+    expect(result).toEqual(expectedOutput);
+  });
+});
