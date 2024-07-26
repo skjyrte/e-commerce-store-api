cf302dd97cab0f968be9474cf9165bfceb82f52e
skjyrte <wasylewiczkrzysztof@gmail.com>
2024-07-26 19:26:02 +0200

    refactor(product): simplify query execution logic
    - Refactored the product routes to use a generic executeQuery utility function.
    - Integrated processSQLProductExtraData within the executeQuery utility.

--- src/routes/product.ts
+++ src/routes/product.ts
@@ -1,29 +1,37 @@
 import express from "express";
-import pool from "../db.js";
 import processSQLProductExtraData from "./routerUtilities/processSQLProductExtraData.js";
-import createResponse from "./routerUtilities/createResponse.js";
+import executeQuery from "./routerUtilities/executeQuery.js";
 
 const router = express.Router();
 
 router.get("/", async (req, res) => {
-  const client = await pool.connect();
-  try {
-    const result = await client.query("SELECT * FROM products");
-    res.status(200).json(result.rows);
-  } catch (error) {
-    console.error("Error executing query", error);
-    res.status(500).json({success: false, message: "Internal Server Error"});
-  } finally {
-    client.release();
-  }
+  const query = `
+    SELECT 
+        products.*, 
+        product_stock.size, 
+        product_stock.count, 
+        product_images.image_url
+        FROM 
+          products
+        LEFT JOIN 
+          product_stock ON products.id = product_stock.product_id
+        LEFT JOIN 
+          product_images ON products.id = product_images.product_id
+       `;
+  await executeQuery(
+    res,
+    query,
+    [],
+    "GET Request Called",
+    "Products not found",
+    processSQLProductExtraData
+  );
 });
 
 router.get("/:id", async (req, res) => {
-  const client = await pool.connect();
   const {id} = req.params;
-  try {
-    const result = await client.query(
-      `SELECT 
+  const query = `
+    SELECT 
         products.*, 
         product_stock.size, 
         product_stock.count, 
@@ -35,24 +43,15 @@ router.get("/:id", async (req, res) => {
         LEFT JOIN 
           product_images ON products.id = product_images.product_id
        WHERE 
-         products.id = $1`,
-      [id]
-    );
-
-    if (result.rows.length > 0) {
-      const processedResult = processSQLProductExtraData(result.rows);
-      res
-        .status(200)
-        .send(createResponse(true, "GET Request Called", processedResult));
-    } else {
-      res.status(404).send(createResponse(false, "Product not found"));
-    }
-  } catch (error) {
-    console.error("Error executing query", error);
-    res.status(500).send(createResponse(false, "Internal server error"));
-  } finally {
-    client.release();
-  }
+         products.id = $1`;
+  await executeQuery(
+    res,
+    query,
+    [id],
+    "GET Request Called",
+    "Products not found",
+    processSQLProductExtraData
+  );
 });
 
 export default router;
