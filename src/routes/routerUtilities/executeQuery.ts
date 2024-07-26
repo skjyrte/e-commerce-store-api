682cc531000ed92c77f7534a211ae4f8bc9e27ad
skjyrte <wasylewiczkrzysztof@gmail.com>
2024-07-26 19:51:03 +0200

    docs(executequery.ts): add tsdoc function explaination

--- src/routes/routerUtilities/executeQuery.ts
+++ src/routes/routerUtilities/executeQuery.ts
@@ -3,6 +3,26 @@ import createResponse from "./createResponse.js";
 
 import pool from "../../db.js";
 
+/**
+ * Executes a SQL query and sends the result as an HTTP response.
+ *
+ * @param res - The Express response object.
+ * @param query - The SQL query string to be executed.
+ * @param params - An array of parameters to be passed to the SQL query.
+ * @param successMessage - A message to be sent in the response on successful execution.
+ * @param notFoundMessage - A message to be sent in the response if no records are found.
+ * @param dbProcessor - A function to process the database rows before sending them in the response.
+ *
+ * @returns void
+ *
+ * This function connects to the database, executes the given query with the provided parameters,
+ * and processes the results using the specified dbProcessor function. It then sends an appropriate
+ * response based on the query outcome.
+ *
+ * - On success, it sends a 200 status code with the processed data.
+ * - If no records are found, it sends a 404 status code with a not found message.
+ * - On error, it sends a 500 status code with an internal server error message.
+ */
 async function executeQuery(
   res: express.Response,
   query: string,
